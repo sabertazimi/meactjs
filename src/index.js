e5ae15ba22db5c2b3c6af4346d188f832ca77ff0
sabertazimi <sabertazimi@gmail.com>
2018-06-15 20:12:38 +0800

    add(diff): create diff algorithm framework

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import style from './index.css';
 const TEXT_ELEMENT_TYPE = 'text element';
 const CHILDREN_PROP = 'children';
 
+let rootInstance = null;
+
 const createTextElement = (value) => {
     return createElement(TEXT_ELEMENT_TYPE, {
         nodeValue: value
     });
-}
+};
 
 const createElement = (type, config, ...args) => {
-    const props = { ...config };
+    const props = { ...config
+    };
     const hasChildren = args.length > 0;
     const rawChildren = hasChildren ? [].concat(...args) : [];
 
@@ -25,7 +28,25 @@ const createElement = (type, config, ...args) => {
     };
 };
 
-const render = (element, parentDOM) => {
+const render = (element, container) => {
+    const prevInstance = rootInstance;
+    const nextInstance = reconcile(container, prevInstance, element);
+    rootInstance = nextInstance;
+};
+
+const reconcile = (parentDOM, instance, element) => {
+    if (instance === null) {
+        const newInstance = instantiate(element);
+        parentDOM.appendChild(newInstance.dom);
+        return newInstance;
+    } else {
+        const newInstance = instantiate(element);
+        parentDOM.replaceChild(newInstance.dom, instance.dom);
+        return newInstance;
+    }
+};
+
+const instantiate = (element) => {
     const {
         type,
         props
@@ -37,41 +58,57 @@ const render = (element, parentDOM) => {
         document.createTextNode('') :
         document.createElement(type);
 
+    updateDOMProperties(dom, [], props);
+
+    // instantiate children
+    const childElements = props.children || [];
+    const childInstances = childElements.map(instantiate);
+
+    // append children
+    const childDOMs = childInstances.map(childInstance => childInstance.dom);
+    childDOMs.forEach(childDOM => dom.appendChild(childDOM));
+
+    // construct instance with attributes and children
+    const instance = {
+        dom,
+        element,
+        childInstances
+    };
+    return instance;
+};
+
+const updateDOMProperties = (dom, prevProps, nextProps) => {
     // add event listeners
     const isListener = name => name.startsWith('on');
-    Object.keys(props).filter(isListener).forEach((name) => {
-        const eventType = name.toLowerCase().substring(2); // trip off 'on'
-        dom.addEventListener(eventType, props[name]);
-    });
-
-    // set properties
     const isAttribute = name => !isListener(name) && name !== CHILDREN_PROP;
-    Object.keys(props).filter(isAttribute).forEach((name) => {
-        dom[name] = props[name];
+
+    // remove prevProps
+    Object.keys(prevProps).filter(isListener).forEach((name) => {
+        const eventType = name.toLowerCase().substring(2); // trip off 'on'
+        dom.removeEventListener(eventType, prevProps[name]);
     });
 
-    // render children
-    const childElements = props.children || [];
-    childElements.forEach(childElement => render(childElement, dom));
+    Object.keys(prevProps).filter(isAttribute).forEach((name) => {
+        dom[name] = null;
+    });
 
-    // append to parent
-    // if (!parentDOM.lastChild) {
-        parentDOM.appendChild(dom);
-    // } else {
-        // parentDOM.replaceChild(dom, parentDOM.lastChild);
-    // }
+    // add nextProps
+    Object.keys(nextProps).filter(isListener).forEach((name) => {
+        const eventType = name.toLowerCase().substring(2); // trip off 'on'
+        dom.addEventListener(eventType, nextProps[name]);
+    });
+
+    Object.keys(nextProps).filter(isAttribute).forEach((name) => {
+        dom[name] = nextProps[name];
+    });
 };
 
 const meactElement = (
     <div id="container">
-      <input value="foo" type="text" />
-      <a href="/bar">bar</a>
-      <span onClick={e => alert("Hi")}>click me</span>
+        <input value="foo" type="text" />
+        <a href="/bar">bar</a>
+        <span onClick={e => alert("Hi")}>click me</span>
     </div>
 );
 
 render(meactElement, document.getElementById('root'));
-
-export {
-    render
-};
